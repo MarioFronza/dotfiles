bd095e5cee81701a74aaccf6df00ef92497d6c45
MarioFronza <mariofronza1@gmail.com>
2025-04-19 10:40:19 -0300

    p10k

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -126,3 +126,8 @@ export SDKMAN_DIR="$HOME/.sdkman"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# To customize prompt, run `p10k configure` or edit ~/Workspaces/github/dotfiles/p10k/p10k.zsh.
+[[ ! -f ~/Workspaces/github/dotfiles/p10k/p10k.zsh ]] || source ~/Workspaces/github/dotfiles/p10k/p10k.zsh
+
+export PATH=$PATH:/usr/local/go/bin
