fc23158019b8fe088ae7b41e30fea67c0468b11f
Mario Fronza <mariofronza1@gmail.com>
2026-09-20 09:53:56 -0300

    Revert "nvim: fix flexoki colors for snacks explorer sidebar"
    This reverts commit 9b16f8d5cbec658ffc425ebbe2e7432b88c14295.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,5 @@
 return {
-  {
-    "kepano/flexoki-neovim",
-    name = "flexoki",
-    priority = 1000,
-    opts = function()
-      local c = require("flexoki.palette").palette()
-      return {
-        highlight_groups = {
-          SnacksNormal = { fg = c["tx-2"], bg = c["bg-2"] },
-          SnacksNormalNC = { fg = c["tx-2"], bg = c["bg-2"] },
-          SnacksWinBar = { fg = c["tx"], bg = c["bg-2"] },
-          SnacksTitle = { fg = c["tx"], bg = c["bg-2"] },
-          SnacksFooter = { fg = c["tx-2"], bg = c["bg-2"] },
-          SnacksWinSeparator = { fg = c["bg-2"], bg = c["bg-2"] },
-        },
-      }
-    end,
-  },
+  { "kepano/flexoki-neovim", name = "flexoki" },
   {
     "LazyVim/LazyVim",
     opts = {
